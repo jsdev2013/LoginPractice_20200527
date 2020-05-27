097873701f6426f2f88a86204ffa1a4f397a664d
JISU LEE <jsnote2013@gmail.com>
2020-05-27 19:51:19 +0900

    로그 / 토스트 사용 예시

--- app/src/main/java/com/jisu/loginpractice_20200527/MainActivity.kt
+++ app/src/main/java/com/jisu/loginpractice_20200527/MainActivity.kt
@@ -2,11 +2,21 @@ package com.jisu.loginpractice_20200527
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        eventBtn.setOnClickListener {
+            Log.d("이벤트 확인", "버튼클릭")
+            Log.e("이벤트 확인","에러로그 발생")
+
+            Toast.makeText(this,"버튼이 클릭됨",Toast.LENGTH_SHORT).show()
+        }
     }
 }
